a5ef5e86a907ce937725a3398d58fa9fa64f64d8
duckplug <okolelovant@gmail.com>
2022-06-07 13:32:13 +0300

    Init checkstyle

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -8,8 +8,8 @@ import java.util.List;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {System.out.println( "Hello World!" );}
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
