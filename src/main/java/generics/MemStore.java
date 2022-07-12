c05ad78c930aa50530c5bb2863b75197523b5a18
duckplug <okolelovant@gmail.com>
2022-07-12 15:42:12 +0300

    5.2.2. Реализовать Store<T extends Base> [#157]

---
+++ src/main/java/generics/MemStore.java
@@ -0,0 +1,44 @@
+package generics;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public final class MemStore<T extends Base> implements Store<T> {
+
+    private final Map<String, T> storage = new HashMap<>();
+
+    @Override
+    public void add(T model) {
+        T t = storage.get(model.getId());
+        if (t == null) {
+            t = storage.put(model.getId(), model);
+        }
+    }
+
+    @Override
+    public boolean replace(String id, T model) {
+        if (storage.containsKey(id)) {
+            storage.put(id, model);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    @Override
+    public boolean delete(String id) {
+        if (storage.containsKey(id)) {
+            storage.remove(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public T findById(String id) {
+        return storage.get(id);
+    }
+}
