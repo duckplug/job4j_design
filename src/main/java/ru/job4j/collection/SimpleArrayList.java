3baaa9efa07ac42774b1de09407e93b870294ff8
duckplug <okolelovant@gmail.com>
2022-07-21 12:38:36 +0300

    1. Динамический список на массиве. [#158 #323330]

--- src/main/java/ru/job4j/collection/SimpleArrayList.java
+++ src/main/java/ru/job4j/collection/SimpleArrayList.java
@@ -1,17 +1,13 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.Objects;
+import java.util.*;
 
 public class SimpleArrayList<T> implements SimpleList<T> {
 
     private T[] container;
-
     private int size;
-
     private int modCount;
+
 
     public SimpleArrayList(int capacity) {
         this.container = (T[]) new Object[capacity];
@@ -19,58 +15,69 @@ public class SimpleArrayList<T> implements SimpleList<T> {
 
     @Override
     public void add(T value) {
-        container[size] = value;
-        size += 1;
+        if (size >= container.length) {
+            increaseArrayLength();
+        }
+        container[size++] = value;
+        modCount += 1;
     }
 
     @Override
     public T set(int index, T newValue) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         T oldValue = container[index];
         container[index] = newValue;
-        return  oldValue;
+        return oldValue;
 
     }
 
     @Override
     public T remove(int index) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         T oldValue = container[index];
-        System.arraycopy(container, index + 1, container, index, 1);
-        container[container.length - 1 - index] = null;
+        System.arraycopy(container, index + 1, container, index, container.length - index - 1);
+        container[container.length - 1] = null;
+        modCount += 1;
+        size--;
         return oldValue;
     }
 
     @Override
     public T get(int index) {
-        Objects.checkIndex(index, container.length);
-        return  container[index];
+        Objects.checkIndex(index, size);
+        return container[index];
     }
 
     @Override
     public int size() {
-        return container.length;
+        return size;
+    }
 
+    public void increaseArrayLength() {
+        container = Arrays.copyOf(container, container.length + 1);
     }
 
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
+            int index = 0;
+            int expectedModCount = modCount;
 
             @Override
             public boolean hasNext() {
-                int expectedModCount = modCount;
-                return true;
-
-
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException("Коллекция был изменена");
+                }
+                return index < size;
             }
 
             @Override
             public T next() {
-                return null;
-
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return container[index++];
             }
-
         };
     }
 }
