597b9ec67562dd396340a8cd22e76129d369438a
duckplug <okolelovant@gmail.com>
2022-08-31 09:33:57 +0300

    3. Переопределить только hashCode [#1003 #337932]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -15,16 +15,7 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
-/**
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User)) return false;
-        User user = (User) o;
-        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
-    }
 
- */
     @Override
     public int hashCode() {
         return Objects.hash(name, children, birthday);
