48ae3e09321fe0adf7db31b3797da484d202a270
duckplug <okolelovant@gmail.com>
2022-11-17 12:16:46 +0300

    0.1. FileOutputStream. [#252488 #361612]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,10 +5,9 @@ import java.io.FileOutputStream;
 public class ResultFile {
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            Integer x;
             for (int i = 1; i <= 9; i++) {
                 for (int j = 1; j <= 9; j++) {
-                    out.write(Integer.toString(i * j).getBytes());
+                    out.write(Integer.toString(j * i).getBytes());
                     out.write(System.lineSeparator().getBytes());
                 }
             }
