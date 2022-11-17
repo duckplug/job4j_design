129250ffdd3683231ab3787555636729b4b5ccfe
duckplug <okolelovant@gmail.com>
2022-11-17 10:32:28 +0300

    2. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -8,23 +8,29 @@ public class SimpleQueue<T> {
     private int countPush;
     private int countPoll;
 
+    //Метод poll() - должен возвращать первое значение и удалять его из коллекции.
     public T poll() {
         if (countPush <= 0 && countPoll <= 0) {
             throw new NoSuchElementException();
         }
-        if (countPoll == 0) {
-            while (countPush > 0) {
-                out.push(in.pop());
+
+        if (countPoll == 0) { // счетчик удаления первого значения
+            while (countPush > 0) { // счетчик помещения значения в конец
+                out.push(in.pop()); // из стека in удаляется первый элемент и помещается в стек out первым
                 countPush--;
                 countPoll++;
             }
+            // цикл работает пока стек in имеет элементы и начинается если в стеке out нет элементов
         }
         countPoll--;
-        return out.pop();
+        return out.pop(); // возвращаем 1-ый элемент из стека out и понижаем счетчик (как только он станет = 0,
+        // начнется опять переброс элементов между стеками)
+
     }
 
+    //Метод push(T value) - помещает значение в конец.
     public void push(T value) {
-        in.push(value);
+        in.push(value); // добавляем элемент в начало стека
         countPush++;
     }
 }
