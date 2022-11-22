033a1b676a976215e6758789a3dff38a10e73ae4
duckplug <okolelovant@gmail.com>
2022-11-22 15:43:07 +0300

    0.1. FileOutputStream. [#252488 #361612]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -2,6 +2,7 @@ package ru.job4j.io;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +18,7 @@ public class LogFilter {
                     rsl.add(line);
                 }
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
