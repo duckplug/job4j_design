75ec7523552720bc03c2caea50f657be2cd86b57
duckplug <okolelovant@gmail.com>
2022-11-23 11:35:52 +0300

    0.3. BufferedReader. [#252489 #361712]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -10,25 +10,23 @@ public class LogFilter {
     public List<String> filter(String file) {
         List<String> rsl = new ArrayList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
-
             for (String line = in.readLine(); line != null; line = in.readLine()) {
                 String[] str = line.split(" ");
                 if ("404".equals(str[str.length - 2])) {
-
                     rsl.add(line);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return rsl;
     }
 
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        System.out.println(log);
-
+        for (String s:log) {
+            System.out.println(s);
+        }
     }
 }
