9ae124c3d8f059d8fbaa77fa01eb921085bedf91
duckplug <okolelovant@gmail.com>
2022-12-02 11:43:41 +0300

    2. Анализ доступности сервера. [#859 #366098]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -5,29 +5,26 @@ import java.io.*;
 public class Analysis {
 
     public void unavailable(String source, String target) {
-        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            boolean err = false;
+        try (BufferedReader read = new BufferedReader(
+                new FileReader(source));
+            PrintWriter out = new PrintWriter(
+                    new BufferedOutputStream(
+                    new FileOutputStream(target)
+                    ))) {
+            boolean work = true;
+            boolean fall;
             for (String line = read.readLine(); line != null; line = read.readLine()) {
                 String[] str = line.split(" ");
                 int i = Integer.parseInt(str[0]);
-                if (i > 300) {
-                    err = true;
-                }
-                if (err) {
-                    try (PrintWriter out = new PrintWriter(
-                            new BufferedOutputStream(
-                                    new FileOutputStream(target)
-                            ))) {
-                        out.println(str[1] + ";");
-                        if (i < 400) {
-                            err = false;
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                fall = i > 300;
+                if (work && fall) {
+                    out.print(str[1] + ";");
+                    work = false;
+                } else if (!work && !fall) {
+                    out.println(str[1] + ";");
+                    work = true;
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
