f72686a08fc15ad7e9aa6f3e999b9596d2546a0a
duckplug <okolelovant@gmail.com>
2022-12-07 11:54:26 +0300

    4.0. File [#252491 #368224]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -1,9 +1,11 @@
 package ru.job4j.io;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class Dir {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File file = new File("d:\\JP\\projects");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
@@ -13,7 +15,7 @@ public class Dir {
         }
         System.out.println(String.format("size : %s", file.getTotalSpace()));
         for (File subfile : file.listFiles()) {
-            System.out.println(String.format(subfile.getName() + " - " + subfile.length()));
+            System.out.println(String.format(subfile.getName()) + " - " + subfile.length());
         }
     }
 }
