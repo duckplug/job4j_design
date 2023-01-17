b48905dfe26ca57f7a6507aef61fd0085b0dc3ef
duckplug <okolelovant@gmail.com>
2023-01-17 10:20:19 +0300

    Регулярные выражения. Метасимволы, квантификаторы, классы символов. [#504918 #381696]

--- src/main/java/ru/job4j/io/RegExSample.java
+++ src/main/java/ru/job4j/io/RegExSample.java
@@ -1,20 +1,17 @@
 package ru.job4j.io;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExSample {
-    public static void main(String[] args) {
-        Pattern patternEng = Pattern.compile("WORD", Pattern.CASE_INSENSITIVE);
-        String wordEng = "word";
-        Matcher matcherEng = patternEng.matcher(wordEng);
-
-        Pattern patternRu = Pattern.compile("СЛОВО", Pattern.CASE_INSENSITIVE);
-        String wordRu = "слово";
-        Matcher matcherRu = patternRu.matcher(wordRu);
-
-
-        System.out.println(matcherEng.matches());
-        System.out.println(matcherRu.matches());
+        public static void main(String[] args) {
+            Pattern pattern = Pattern.compile("\\S+@\\S+\\.\\S+");
+            String text = "peter-2022@example.com example65@mail_box.ru 123_45@example-mailbox.com";
+            Matcher matcher = pattern.matcher(text);
+            while (matcher.find()) {
+                System.out.println("Найдено совпадение: " + matcher.group());
+            }
+        }
     }
-}
