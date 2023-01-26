654eb2ea1869fa1199dc2c1e67fb6e001a54b34f
duckplug <okolelovant@gmail.com>
2023-01-26 15:45:14 +0300

    11. Поиск дубликатов [#315066 #384536]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -3,9 +3,22 @@ package ru.job4j.io.duplicates;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class DuplicatesFinder {
+    private void  showDuplicates() throws IOException {
+        DuplicatesVisitor dv = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("./"), dv);
+        for (String str:dv.getDuplicates().keySet()) {
+            System.out.printf("%s - %s \n", dv.getDuplicates().get(str).getName(), str);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        DuplicatesFinder df = new DuplicatesFinder();
+        df.showDuplicates();
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,20 +5,27 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private HashMap<FileProperty, Path> dupplicatesMap = new HashMap<>();
+    public HashMap<FileProperty, Path> map = new HashMap<>();
+    public HashMap<String, FileProperty> duplicates = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProp = new FileProperty(file.toFile().length(), file.toFile().getName());
-        if (!dupplicatesMap.containsKey(fileProp)) {
-            dupplicatesMap.put(fileProp, file);
+        if (!map.containsKey(fileProp)) {
+            map.put(fileProp, file);
         } else {
-            System.out.printf("%s - %s\n\r", fileProp.getName(), file.toFile().getAbsolutePath());
+            duplicates.put(file.toFile().getAbsolutePath(), fileProp);
         }
-        return super.visitFile(file, attrs);
+        return FileVisitResult.CONTINUE;
+    }
+
+    public HashMap<String, FileProperty> getDuplicates() {
+        return duplicates;
     }
 }
