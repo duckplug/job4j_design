7de32b2355e080b8d299597e06091195ee4da4a6
duckplug <okolelovant@gmail.com>
2023-01-27 13:04:38 +0300

    11. Поиск дубликатов [#315066 #384536]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,14 +5,14 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
+
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    public HashMap<FileProperty, Path> map = new HashMap<>();
-    public HashMap<String, FileProperty> duplicates = new HashMap<>();
+    public Map<FileProperty, Path> map = new HashMap<>();
+    public Map<String, FileProperty> duplicates = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
@@ -21,11 +21,13 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
             map.put(fileProp, file);
         } else {
             duplicates.put(file.toFile().getAbsolutePath(), fileProp);
+            duplicates.put(map.get(fileProp).toString(), fileProp);
+            map.remove(fileProp);
         }
         return FileVisitResult.CONTINUE;
     }
 
-    public HashMap<String, FileProperty> getDuplicates() {
+    public Map<String, FileProperty> getDuplicates() {
         return duplicates;
     }
 }
