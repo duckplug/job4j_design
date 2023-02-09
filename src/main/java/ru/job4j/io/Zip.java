0d1a15414055cf024e546f81f7fcaeb693c3f307
duckplug <okolelovant@gmail.com>
2023-02-09 16:29:22 +0300

    14. Архивировать проект [#861 #389665]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -3,17 +3,20 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<Path> sources, File target) throws IOException {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (Path file : sources) {
                 zip.putNextEntry(new ZipEntry(file.toString()));
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -27,24 +30,25 @@ public class Zip {
             e.printStackTrace();
         }
     }
-
-    public static List<Path> fillSources(String[] args) throws IOException {
-        ArgsName argsName = ArgsName.of(args);
-        return Search.search(Paths.get(argsName.get("d")), p -> p.toFile().getName().endsWith(argsName.get("e")));
-    }
-
-    public static void main(String[] args) throws IOException {
+   
+    private static List<Path> validateArgs(String[] args) throws IOException  {
         if (args.length != 3) {
             throw new IllegalArgumentException("Ошибка в количестве аргументов");
         }
-        ArgsName argsName = ArgsName.of(args);
-        File file  = new File(argsName.get("d"));
+        ArgsName argsname = ArgsName.of(args);
+        List<Path> list =   Search.search(Paths.get(argsname.get("d")), p -> p.toFile().getName().endsWith(argsname.get("e")));
+        File file  = new File(argsname.get("d"));
         if (!file.isDirectory()) {
             throw new IllegalArgumentException("Директория - " + args[0] + " не существует");
         }
+        return list;
+    }
 
+    public static void main(String[] args) throws IOException {
+        ArgsName argsName = ArgsName.of(args);
+        validateArgs(args);
         Zip zip = new Zip();
-        zip.packFiles(fillSources(args), new File("./pom.zip")
-        );
+        zip.packFiles(validateArgs(args), new File(argsName.get("o")));
+
     }
 }
