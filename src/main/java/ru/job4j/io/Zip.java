126825dbb73dc9f86d30e76b7e11d53cad8d19c4
duckplug <okolelovant@gmail.com>
2023-02-13 09:52:40 +0300

    14. Архивировать проект [#861 #389665]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -7,7 +7,8 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-public class Zip {    public void packFiles(List<Path> sources, File target) {
+public class Zip {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
             for (Path file : sources) {
                 zip.putNextEntry(new ZipEntry(file.toString()));
@@ -17,34 +18,23 @@ public class Zip {    public void packFiles(List<Path> sources, File target) {
         }
     }
 
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-   
-    private static List<Path> validateArgs(String[] args) throws IOException  {
+    private static void validateArgs(String[] args) {
         if (args.length != 3) {
             throw new IllegalArgumentException("Ошибка в количестве аргументов");
         }
         ArgsName argsname = ArgsName.of(args);
-        List<Path> list =   Search.search(Paths.get(argsname.get("d")), p -> !p.toFile().getName().endsWith(argsname.get("e")));
         File file  = new File(argsname.get("d"));
         if (!file.isDirectory()) {
             throw new IllegalArgumentException("Директория - " + args[0] + " не существует");
         }
-        return list;
     }
 
     public static void main(String[] args) throws IOException {
-        ArgsName argsName = ArgsName.of(args);
+        validateArgs(args);
+        ArgsName argsname = ArgsName.of(args);
+        List<Path> list =   Search.search(Paths.get(argsname.get("d")), p -> !p.toFile().getName().endsWith(argsname.get("e")));
         validateArgs(args);
         Zip zip = new Zip();
-        zip.packFiles(validateArgs(args), new File(argsName.get("o")));
+        zip.packFiles(list, new File(argsname.get("o")));
     }
 }
