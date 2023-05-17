1128de6cce27378567264421d0e8ba2543f898d3
duckplug <okolelovant@gmail.com>
2023-05-17 16:36:51 +0300

    2. Формат JSON [#313164]

---
+++ src/main/java/ru/job4j/serialization/java/CarsXML.java
@@ -0,0 +1,26 @@
+package ru.job4j.serialization.java;
+
+import java.util.Arrays;
+
+public class CarsXML {
+
+    private final boolean sportCar;
+    private final int maxSpeed;
+    private final String model;
+    private final String[] id;
+    private final EngineCarXML engine;
+
+    public CarsXML(boolean sportCar, int maxSpeed, String model, String[] id, EngineCarXML engine) {
+        this.sportCar = sportCar;
+        this.maxSpeed = maxSpeed;
+        this.model = model;
+        this.id = id;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "CarsJson{" + "sportCar=" + sportCar + ", maxSpeed=" + maxSpeed + ", model='" + model + '\'' + ", id=" + Arrays.toString(id) + ", engine=" + engine + '}';
+    }
+}
+
---
+++ src/main/java/ru/job4j/serialization/java/EngineCarXML.java
@@ -0,0 +1,16 @@
+package ru.job4j.serialization.java;
+
+public class EngineCarXML {
+    private final String modelEng;
+    private final double capacity;
+
+    public EngineCarXML(String model, double capacity) {
+        this.modelEng = model;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "EngineCarJson{" + "model='" + modelEng + '\'' + ", capacity=" + capacity + '}';
+    }
+}
