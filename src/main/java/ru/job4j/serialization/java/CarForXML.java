d092199ab88864d4456c32947003bf5dca7de65a
duckplug <okolelovant@gmail.com>
2023-05-18 12:36:48 +0300

    JAXB. Преобразование XML в POJO.

--- src/main/java/ru/job4j/serialization/java/CarForXML.java
+++ src/main/java/ru/job4j/serialization/java/CarForXML.java
@@ -1,8 +1,51 @@
 package ru.job4j.serialization.java;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+
 public class CarForXML {
-    public static void main(String[] args) {
-        CarsXML carsXML = new CarsXML(false, 200, "SL", new String[] {"1", "2", "C"}, new EngineCarXML("forSL", 2.2));
-        System.out.println(carsXML);
+
+    public static void main(String[] args) throws IOException, JAXBException {
+        CarsXML car = new CarsXML(false, 200, "ML", new String[] {"1", "2"}, new EngineCarXML("SC", 1.6));
+        /**
+         * Получаем контекст для доступа к АПИ
+         */
+        JAXBContext context = JAXBContext.newInstance(CarsXML.class);
+        /**
+         *  Создаем сериализатор
+         */
+        Marshaller marshaller = context.createMarshaller();
+        /**
+         * Указываем, что нам нужно форматирование
+         */
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            /**
+             * Сериализуем
+             */
+            marshaller.marshal(car, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+        /**
+         * Для десериализации нам нужно создать десериализатор
+         */
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            /**
+             *  десериализуем
+             */
+            CarsXML result = (CarsXML) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
+
     }
 }
+
+
--- src/main/java/ru/job4j/serialization/java/CarsXML.java
+++ src/main/java/ru/job4j/serialization/java/CarsXML.java
@@ -1,14 +1,33 @@
 package ru.job4j.serialization.java;
 
 import java.util.Arrays;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringWriter;
+
+@XmlRootElement(name = "carXML")
+@XmlAccessorType(XmlAccessType.FIELD)
 
 public class CarsXML {
 
-    private final boolean sportCar;
-    private final int maxSpeed;
-    private final String model;
-    private final String[] id;
-    private final EngineCarXML engine;
+    @XmlAttribute
+    private boolean sportCar;
+
+    @XmlAttribute
+    private int maxSpeed;
+
+    @XmlAttribute
+    private String model;
+
+    @XmlElementWrapper(name = "CarId")
+    @XmlElement(name = "id")
+    private  String[] id;
+    private  EngineCarXML engine;
+
+    public CarsXML() {
+    }
 
     public CarsXML(boolean sportCar, int maxSpeed, String model, String[] id, EngineCarXML engine) {
         this.sportCar = sportCar;
