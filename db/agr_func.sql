24fdfcef25aae276c2696050bbc96b2862c31cdf
duckplug <okolelovant@gmail.com>
2023-08-25 18:35:38 +0300

    2. Группировка и агрегатные функции [#373618]

---
+++ db/agr_func.sql
@@ -0,0 +1,31 @@
+create table devices(
+    id serial primary key,
+    name varchar(255),
+    price float
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table devices_people(
+    id serial primary key,
+    device_id int references devices(id),
+    people_id int references people(id)
+);
+
+insert into people(name) values ('Иван');
+insert into people(name) values ('Сергей');
+insert into people(name) values ('Павел');
+insert into devices(name, price) values ('Bike', 25000);
+insert into devices(name, price) values ('Computer', 12000);
+insert into devices(name, price) values ('Phone', 80000);
+insert into devices_people(device_id, people_id) values (1, 1);
+insert into devices_people(device_id, people_id) values (2, 2);
+insert into devices_people(device_id, people_id) values (3, 3);
+
+select avg(price) from devices;
+select pp.name, avg(ds.price) from devices ds 
+join people pp on pp.id = ds.id group by pp.name
+having avg(ds.price) > 12000;
