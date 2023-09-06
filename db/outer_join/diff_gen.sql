0ee87a1cfc4c9bc31b0c9c0e280ca24d01638073
duckplug <okolelovant@gmail.com>
2023-09-06 16:08:02 +0300

    0. Join [#6862 #442367]

--- db/outer_join/diff_gen.sql
+++ db/outer_join/diff_gen.sql
@@ -11,4 +11,4 @@ insert into teens(name, gender) values('Kate', 'female');
 insert into teens(name, gender) values('Rose', 'female');
 insert into teens(name, gender) values('Ann', 'female');
 
-select t1.name, t2.name, t1.gender, t2.gender from teens t1 cross join teens t2 where t1.gender not like t2.gender;
+select t1.name, t2.name, t1.gender, t2.gender from teens t1 cross join teens t2 where t1.gender not like t2.gender and t1.gender = 'male';
