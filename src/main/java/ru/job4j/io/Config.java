422638f5705866db95fc1f2061784a2f5d616841
duckplug <okolelovant@gmail.com>
2023-11-02 14:38:03 +0300

    0. JDBC [#6863]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -1,9 +1,12 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Config {
 
@@ -16,14 +19,16 @@ public class Config {
 
     public void load() {
         try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
-            StringBuilder text = new StringBuilder(read.readLine());
-            int point = text.indexOf("=");
-            if (point < 1 || point == text.length() - 1) {
-                throw new IllegalArgumentException("ошибка шаблона пары ключ - значение");
+            List<String> arr = read.lines().collect(Collectors.toList());
+            for (String text : arr) {
+                int point = text.indexOf("=");
+                if (point < 1 || point == text.length() - 1) {
+                    throw new IllegalArgumentException("ошибка шаблона пары ключ - значение");
+                }
+                String k = text.substring(0, point);
+                String v = text.substring(point + 1, text.length());
+                values.put(k, v);
             }
-            String k = text.substring(0, point);
-            String v = text.substring(point + 1, text.length());
-            values.put(k, v);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -45,6 +50,7 @@ public class Config {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Config("app.properties"));
+        Config conf = new Config("./data/app.properties");
+        conf.load();
     }
 }
---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,24 @@
+package ru.job4j.jdbc;
+
+import ru.job4j.io.Config;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        /**
+         * Class.forName("org.postgresql.Driver");
+         */
+        Config conf = new Config("D:\\JP\\job4j_design\\data\\app.properties");
+        conf.load();
+        try (Connection connection = DriverManager.getConnection(conf.value("url"), conf.value("login"), conf.value("password"))) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
